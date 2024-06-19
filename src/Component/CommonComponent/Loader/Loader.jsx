6d2ac20828582bbf7941ee254ffadc61ec4260d6
shubham24task <shubham@24task.com>
2024-06-19 17:58:49 +0530

    code

---
+++ src/Component/CommonComponent/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loder.css'
+function Loader() {
+  return (
+    <div class="loading-state">
+    <div class="loading"></div>
+    </div>
+  )
+}
+
+export default Loader
