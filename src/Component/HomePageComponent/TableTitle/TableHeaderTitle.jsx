784904a1a8a22192d5675ccda03646f9e70abb06
shubham24task <shubham@24task.com>
2024-06-20 18:26:58 +0530

    changes

--- src/Component/HomePageComponent/TableTitle/TableHeaderTitle.jsx
+++ src/Component/HomePageComponent/TableTitle/TableHeaderTitle.jsx
@@ -9,8 +9,8 @@ import { Image } from 'react-bootstrap';
 function TableHeaderTitle(props) {
     return (
         <div className={`maintitle ${props.className}`}>
-            <h1><Image src={props.titleicon}></Image>{props.title}</h1>
-            <Link to={props.link}><Image src={props.profileimg} onClick={props.handleShow}></Image></Link>
+            <h1 onClick={props.handleShow}><Image src={props.titleicon}></Image>{props.title}</h1>
+            <Link to={props.link}><Image src={props.profileimg} ></Image></Link>
         </div>
     );
 }
